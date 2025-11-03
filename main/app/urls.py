1cd20f2c8dbe43013f477d7c70bb3756327149d2
Don Soby <donkiran2255@gmail.com>
2025-11-03 17:41:14 +0530

    Template work and admikn work completed

--- main/app/urls.py
+++ main/app/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('admin/login/', views.admin_login, name='admin_login'),
     path('admin/dashboard/', views.admindashboard, name='admin_dashboard'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
 ]
--- main/app/views.py
+++ main/app/views.py
@@ -1,28 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+
 
 # Create your views here.
 
 
+@never_cache
 def login(request):
     return render(request, 'login.html')
 
 
+
+@never_cache
 def signup(request):
+    if user.is_authenticated:
+        return render(request, 'dashboard.html')
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('login')
     return render(request, 'signup.html')
 
 
-def home(request):
-    return HttpResponse("Welcome to the Home Page!")
 
-
+@never_cache
 def admin_login(request):
     return render(request, 'admin.html')
 
 
+@login_required
+@never_cache
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    if request.user.is_authenticated:
+        return render(request, 'dashboard.html')
+    else:
+        return render(request, 'login.html')
 
 
+@login_required
+@never_cache
 def admindashboard(request):
     return render(request, 'admin-dashboard.html')
